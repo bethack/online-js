2c9d3bba0f6d6618220ab6aacbcbd359df0c869f
Vladimir Severov <vsev.s@mail.ru>
2019-06-07 00:18:03 +0300

    [ADD] input form added

--- main.js
+++ main.js
@@ -1,30 +1,39 @@
 (() => {
+    "use strict";
+
+    function exec(code) {
+        eval(code)
+    }
+
+    let previousLog = console.log;
+    let previousError = console.error;
+
     const app = document.getElementById('application');
+
+    const inputCode = document.getElementById('input-code');
+    const execBtn = document.getElementById('exec-btn');
+    execBtn.addEventListener('click', () => exec(inputCode.value));
+
     const textarea = document.createElement('div');
     textarea.classList = 'console';
-    let previosLog = console.log;
+
     console.log = (message) => {
         const row = document.createElement('div');
         row.textContent = `> ${message}`;
         textarea.appendChild(row);
-        previosLog.apply(console);
-    }
-    let previosError = console.error;
+        previousLog.apply(console);
+    };
+
     console.error = (err) => {
         const row = document.createElement('div');
         row.textContent = `> ${err}`;
         row.classList = 'error';
         textarea.appendChild(row);
-        previosError.apply(console);
-    }
+        previousError.apply(console);
+    };
+
     app.appendChild(textarea);
-    console.log("HELLO");
-    console.error("404");
-    console.log("HELLO");
-    console.log("HELLO");
-    console.log("HELLO");
-    console.log("HELLO");
-    console.log("HELLO");
-    console.error("404");
-    console.log("HELLO");
+
+    exec('console.log("HELLO");');
+    exec('console.error("404");');
 })();
